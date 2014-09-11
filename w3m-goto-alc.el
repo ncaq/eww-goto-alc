b8aa2aa148dfee1c0f86db7308bc8b33ee4319d6
ncaq <ncaq@ncaq.net>
2014-09-11 16:09:22 +0900

    change minibuffer variable

--- w3m-goto-alc.el
+++ w3m-goto-alc.el
@@ -11,8 +11,10 @@
 (defun alc-word-url (word)
   (concat "http://eow.alc.co.jp/search?q=" word "#resultsList"))
 
+(defvar w3m-goto-alc-history)
+
 (defun w3m-goto-alc (word)
-  (interactive (list (read-from-minibuffer "" (thing-at-point 'word))))
+  (interactive (list (read-from-minibuffer "" (thing-at-point 'word) nil nil 'w3m-goto-alc-history)))
   (w3m-browse-url-other-window (alc-word-url word)))
 
 (provide 'w3m-goto-alc)
