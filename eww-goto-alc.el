71e14c544d035fe39f3b8d20ffddbd49c5c9cbda
ncaq <ncaq@ncaq.net>
2015-02-28 03:48:53 +0900

    英辞郎 Proに対応したけどewwでログインする方法がない

--- eww-goto-alc.el
+++ eww-goto-alc.el
@@ -2,20 +2,30 @@
 
 (require 'eww)
 
-(defun eww-browse-url-other-window (url &optional newwin)
-  (if (one-window-p)
-      (split-window-right))
-  (other-window 1)
-  (eww-browse-url url newwin))
+(defcustom alc-eow-use-pro nil
+  "英辞郎Proにアクセスする"
+  :type 'boolean
+  :group 'eww-goto-alc)
+
+(defun alc-search-url ()
+  (if alc-eow-use-pro
+      "http://eowpf.alc.co.jp/search?q="
+    "http://eow.alc.co.jp/search?q="))
+
+(defun alc-content-id ()
+  (if alc-eow-use-pro
+      "#maincontent"
+    "#resultsArea"))
 
 (defun alc-word-url (word)
-  (concat "http://eow.alc.co.jp/search?q=" word "#resultsList"))
+  (concat (alc-search-url) word (alc-content-id)))
 
 (defvar eww-goto-alc-history)
 
 ;;;###autoload
 (defun eww-goto-alc (word)
   (interactive (list (read-string "" (thing-at-point 'word) 'eww-goto-alc-history)))
-  (eww-browse-url-other-window (alc-word-url word)))
+  (eww-browse-url (alc-word-url word) t) ;_new-window option is supported by Emacs-25
+  )
 
 (provide 'eww-goto-alc)
